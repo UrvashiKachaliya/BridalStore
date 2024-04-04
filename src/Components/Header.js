d8b23a0c56dc2dd8599bc38bfea95fde7b231705
Urvashi Kachaliya <kachaliyaurvashi@gmail.com>
2024-04-04 15:00:12 +0530

    slider done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,7 +11,7 @@ function Header() {
   return (
   <Navbar expand="lg" className="p-3" style={{ backgroundColor: "white", boxShadow: "rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px" }} >
       <Container fluid>
-        <Navbar.Brand href="#home" className='fw-semibold mx-auto fs-2 text-black'>BRIDO</Navbar.Brand>
+        <Navbar.Brand href="#home" className='fw-semibold fs-2 text-black'>BRIDO</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
--- src/Components/SampleCard.js
+++ src/Components/SampleCard.js
@@ -6,7 +6,7 @@ export default function SampleCard(props) {
     <Container>
       <Card className='cardp' style={{ width: '100%', border: 'none', alignItems: 'center', transition: 'box-shadow 0.3s ease' }}>
         <div style={{ width: '100%' }}>
-          <img src={props.imgsrc} alt='image1' className='img mt-5 rounded-2' style={{ width: '100%', maxHeight: '25rem', objectFit: 'cover' }} />
+          <img src={props.imgsrc} alt='image1' className='img mt-5 rounded-2' style={{ width: '100%', objectFit: 'cover' }} />
           <div className='text-center'>
             <p className='fw-semibold mt-3'>{props.title}</p>
             <p className='fw-semibold'>{props.price}</p>
